5abeb2a2d3cc396bad07905fbbf0ff1142151a63
elimarayag <marayagelisha@gmail.com>
2023-12-27 00:53:23 +0800

    modified

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
   
+    let currentIndex = 0;
+    const images = document.querySelectorAll('.profile-image');
+
+    function showNextImage() {
+      images[currentIndex].style.display = 'none';
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].style.display = 'block';
+    }
+
+    showNextImage();
+    setInterval(showNextImage, 1000); 
